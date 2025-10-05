2ec825ac51d059d27c606a4e133a62de599b9de6
Thiwanka <janiththiwanka2002@gmail.com>
2025-10-05 17:21:50 +0530

    Create axios.js

---
+++ farmconnect-frontend/src/utils/axios.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+// Create axios instance
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8081', // Default to user service
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request interceptor - Add token to all requests
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor - Handle errors
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // Unauthorized - Clear token and redirect to login
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
